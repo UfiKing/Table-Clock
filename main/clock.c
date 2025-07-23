de5a535250e82ef56d880fe3b83daedea744da43
Ufi <jure@hodak.net>
2025-07-23 15:06:58 +0200

    Blink the number one on first digit

--- main/clock.c
+++ main/clock.c
@@ -17,23 +17,44 @@
 #define G 27
 #define DP 12
 
-
+#define LED 2
 void app_main(void) { 
   const char *tag = "clock";
 
   ESP_LOGI(tag, "Initializing variables");
-
-
-  const uint8_t hi = 1, lo = 0;
+  
+  const uint8_t digitON = 0, digitOFF = 1, segmentON = 1, segmentOFF = 0;
   const TickType_t duration = 500 / portTICK_PERIOD_MS;
+  
+  const uint8_t segmentPins[] = {A, B, C, D, E, F, G, DP};
+  const uint8_t digitPins[] = {D1, D2, D3, D4};
+
+  ESP_LOGI(tag, "Configuring GPIO pins");
+  for (int i = 0; i < 8; i++){
+    gpio_reset_pin(segmentPins[i]);
+    gpio_set_direction(segmentPins[i], GPIO_MODE_OUTPUT);
+  }
+
+  for (int i = 0; i < 4; i++){
+    gpio_reset_pin(digitPins[i]);
+    gpio_set_direction(digitPins[i], GPIO_MODE_OUTPUT);
+    gpio_set_level(digitPins[i], digitOFF);
+  }
+
 
   gpio_reset_pin(LED);
   gpio_set_direction(LED, GPIO_MODE_OUTPUT);
 
   while (1){
-    gpio_set_level(LED, hi);
+    gpio_set_level(LED, 1);
+    gpio_set_level(D1, digitON);
+    gpio_set_level(B, segmentON);
+    gpio_set_level(C, segmentON);
     vTaskDelay(duration);
-    gpio_set_level(LED, lo);
+    gpio_set_level(D1, digitOFF);
+    gpio_set_level(B, segmentOFF);
+    gpio_set_level(C, segmentOFF);   
+    gpio_set_level(LED, 0);
     vTaskDelay(duration);
 
   }
